2ba26b15b32cc9cd060de806d628d20f692334dc
Vitaly <onebeattrue@gmail.com>
2024-01-14 09:55:32 +0300

    Builder

---
+++ src/main/kotlin/builder/Main.kt
@@ -0,0 +1,16 @@
+package builder
+
+/**
+ * Строитель (Builder) - шаблон проектирования,
+ * который инкапсулирует создание объекта и позволяет разделить его на различные этапы.
+ */
+
+fun main() {
+    val student = Student.Builder()
+        .name("Ivan")
+        .age(20)
+        .speciality("programming")
+        .build()
+
+    println(student)
+}
